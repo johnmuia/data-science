3ae3e38895153dda888fb8941314cfc54c3f7c23
John Muia <muiajonn@gmail.com>
2021-11-14 13:12:14 +0300

    find mean, median and std deviation
    Signed-off-by: John Muia <muiajonn@gmail.com>

--- data-analysis.py
+++ data-analysis.py
@@ -1,5 +1,7 @@
+from numpy.core.fromnumeric import mean
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 alumni = pd.read_csv('alumni.csv')
 #print(alumni.describe())
@@ -7,9 +9,15 @@ def clean_currency(curr):
     return float(curr.replace(',', '').replace('$', ''))
 
 alumni['Savings'] = alumni['Savings ($)'].agg(clean_currency)
-print(alumni['Gender'].value_counts())
+#print(alumni['Gender'].value_counts())
 
 #Replace "M" with "Male"
+alumni['Gender'] = alumni['Gender'].str.replace(r'^M$', 'Male', regex=True)
 
-
-#print(alumni['Savings'])
+#print(alumni['Gender'].value_counts())
+#Find the mean, median and stardard deviation
+salary = alumni['Salary'].agg([np.mean, np.median, np.std])
+#print(salary)
+#print(alumni['Fee'].value_counts())
+over_15000 = alumni[alumni['Fee'] >=15000]
+print(over_15000)
