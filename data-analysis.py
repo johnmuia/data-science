02673d560472fc5228e9129622d98f06d16aa003
John Muia <muiajonn@gmail.com>
2021-11-14 12:40:20 +0300

    add some more code for analysis
    Signed-off-by: John Muia <muiajonn@gmail.com>

--- data-analysis.py
+++ data-analysis.py
@@ -1,3 +1,15 @@
 import pandas as pd
 import numpy as np
 
+alumni = pd.read_csv('alumni.csv')
+#print(alumni.describe())
+def clean_currency(curr):
+    return float(curr.replace(',', '').replace('$', ''))
+
+alumni['Savings'] = alumni['Savings ($)'].agg(clean_currency)
+print(alumni['Gender'].value_counts())
+
+#Replace "M" with "Male"
+
+
+#print(alumni['Savings'])
